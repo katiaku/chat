15468fbaaaff6e75f033c23b1b0f26ba6e896750
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-06-20 13:02:29 +0200

    Add client to server communication

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
     })
 })
 
