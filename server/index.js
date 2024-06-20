0c10c2d06d85d5958f782cb5b4ed37c87acca8e3
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-06-20 16:13:10 +0200

    Generate users

--- server/index.js
+++ server/index.js
@@ -25,6 +25,7 @@ await db.execute(`
     CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         content TEXT
+        user TEXT
     )
 `);
 
@@ -36,30 +37,32 @@ io.on('connection', async (socket) => {
     });
 
     socket.on('chat message', async (msg) => {
-        let result
+        let result;
+        const username = socket.handshake.auth.username ?? 'anonymous';
+        console.log({ username });
 
         try {
             result = await db.execute({
-                sql: 'INSERT INTO messages (content) VALUES (:msg)',
-                args: { msg }
+                sql: 'INSERT INTO messages (content, user ) VALUES (:msg, :username)',
+                args: { msg, username }
             })
         } catch (e) {
             console.error(e)
             return
         }
 
-        io.emit('chat message', msg, result.lastInsertRowid.toString());
+        io.emit('chat message', msg, result.lastInsertRowid.toString(), username);
     });
 
     if (!socket.recovered) {
         try {
             const results = await db.execute({
-                sql: 'SELECT id, content FROM messages WHERE id > ?',
+                sql: 'SELECT id, content, user FROM messages WHERE id > ?',
                 args: [socket.handshake.auth.serverOffset ?? 0]
             })
 
             results.rows.forEach(row => {
-                socket.emit('chat message', row.content, row.id.toString());
+                socket.emit('chat message', row.content, row.id.toString(), row.user);
             })
         } catch (e) {
             console.error(e);
